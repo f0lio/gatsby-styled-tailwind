ef61b230bb7a8fb894156aa2516c99ef30b42851
f0lio <36214338+f0lio@users.noreply.github.com>
2021-08-25 20:37:28 +0100

    setup styled-tailwind

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,26 @@
 import React from "react";
+import tw, { styled } from "twin.macro";
+
+const Container = styled.div`
+  ${tw`flex flex-col p-10 border `}
+`;
+
+// shorthand version
+const Text = tw.text`
+  text-2xl  py-2	
+`;
+
+const Button = tw.button`
+  p-3 max-w-max	 cursor-pointer text-lg font-semibold text-white bg-purple-800 border-none rounded-lg shadow-md hover:bg-white hover:text-purple-700
+`;
 
 function Home() {
-  return <div>Hola</div>;
+  return (
+    <Container>
+      <Text>Tailwind classes into Styled Components.</Text>
+      <Button>Hover!</Button>
+    </Container>
+  );
 }
 
 export default Home;
